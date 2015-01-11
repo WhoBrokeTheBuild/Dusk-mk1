f80660d73b52168819bf0452bc16e0b7dda072d7
Stephen Lane-Walsh <sdl.slane@gmail.com>
2015-01-10 20:52:19 -0500

    - Updated C::B Project and added SDL2 references\n- Fixed missing std::string problem in ImportPNG.h

--- ResourceManager/ImportPNG.h
+++ ResourceManager/ImportPNG.h
@@ -1,7 +1,11 @@
 #ifndef DUSK_IMPORT_PNG_H
 #define DUSK_IMPORT_PNG_H
 
+#include <string>
+
 #include "InterTexture.h"
+
+using std::string;
 
 bool ImportPNG( const string& filename, InterTexture* pTexture );
 

4ab2556e05db1589a261476bef54e3044f1f42df
Stephen Lane-Walsh <sdl.slane@gmail.com>
2015-03-03 22:48:53 -0500

    Added linter support files, and began updating Material.cpp

--- Dusk/Graphics/Material.cpp
+++ Dusk/Graphics/Material.cpp
@@ -6,5 +6,18 @@ bool Dusk::Graphics::Material::init( const string& name,
                                      const float& specular, const float& transparency,
                                      const vec3& ambientColor, const vec3& diffuseColor, const vec3& specularColor )
 {
+    m_Name = name;
+    m_DiffuseMap = diffuseMap;
+    m_AmbientMap = ambientMap;
+    m_SpecularMap = specularMap;
+    m_SpecularHilightMap = specularHilightMap;
+    m_AlphaMap = alphaMap;
+    m_BumpMap = bumpMap;
+    m_Specular = specular;
+    m_Transparency = transparency;
+    m_AmbientColor = ambientColor;
+    m_DiffuseColor = diffuseColor;
+    m_SpecularColor = specularColor;
 
+    return true;
 }

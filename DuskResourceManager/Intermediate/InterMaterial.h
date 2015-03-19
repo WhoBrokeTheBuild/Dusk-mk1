d155b77512f8c98cdb5e3946bcf32e76d07c65be
Stephen Lane <stephen.lane@mymail.champlain.edu>
2015-03-19 10:45:05 -0400

    Renamed DuskRC/ to DuskResourceManager/ for windows compatibility and Updated Makefile. Updated Documentation for Log.h

---
+++ DuskResourceManager/Intermediate/InterMaterial.h
@@ -0,0 +1,33 @@
+#ifndef DUSK_INTER_MATERIAL_H
+#define DUSK_INTER_MATERIAL_H
+
+#include <Arc/ManagedObject.h>
+
+#include <Util/Math.h>
+
+class InterMaterial :
+	public Arc::ManagedObject
+{
+public:
+
+	virtual inline string getClassName(void) const { return "Intermediate Material"; }
+
+	string  Name;
+
+    string  DiffuseMap,
+            AmbientMap,
+            SpecularMap,
+            SpecularHilightMap,
+            AlphaMap,
+            BumpMap;
+
+	float   Specular,
+            Transparency;
+
+	vec3    AmbientColor,
+            DiffuseColor,
+            SpecularColor;
+
+};
+
+#endif // DUSK_INTER_MATERIAL_H

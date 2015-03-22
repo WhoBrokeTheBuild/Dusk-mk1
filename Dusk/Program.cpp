f3bb3f4f37ec5a8a1764c483a31f53fe7cfdb04b
Stephen Lane-Walsh <sdl.slane@gmail.com>
2015-03-22 02:46:47 -0400

    Updated VS projects

--- Dusk/Program.cpp
+++ Dusk/Program.cpp
@@ -164,7 +164,7 @@ bool Dusk::Program::load()
     mp_Cube->init();
 
     Model* pCubeModel = New Model();
-    pCubeModel->load("Assets/Models/mdl_cube.dskm");
+    pCubeModel->load("Assets/Models/mdl_cube.obj");
 
     mp_Cube->addModel(pCubeModel);
 
